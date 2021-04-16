88b0b319947e059905522bb751f1f0534667865c
Cooper Jackson <davisjaxon@gmail.com>
2021-04-16 15:57:25 -0700

    finished assignment and added some fun, unnecessary stlying

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character.js'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,66 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // let url = 'https://swapi.dev/api/people/'
+
+  const [characters, setCharacters] = useState({})
+  // const [homeworlds, setHomeworlds] = useState({})
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setCharacters(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  // useEffect(() => {
+    // axios.get('https://swapi.dev/api/planets/')
+    // .then(res => {
+    //   setHomeworlds(res.data)
+    // })
+    // .catch(err => {
+    //   console.log(err)
+    // })
+  // }, [])
+
+  // useEffect(() => {
+  //   Array.from(characters).forEach(element => {
+  //     axios.get(element.homeworld)
+  //     .then(res => {
+  //       setHomeworlds(res.data)  
+  //       // console.log(res)
+  //     })
+  //   })
+  // }, [characters])
+
+  // console.log(homeworlds)
+
+//   axios.get('https://swapi.dev/api/people/')
+//   .then(res => {
+//     setCharacters(res.data)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+// }, [])
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className="app">
+
+      <div className="crawl">
+
+        <h1 className="header">Characters</h1>
+
+        {Array.from(characters).map((character) => (
+          <Character character={character}/>
+        ))}
+
+        <p align-text="center">(this loops)</p>
+
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,83 @@
 // Write your Character component here
+import React from 'react';
+import styled from "styled-components"
+import axios from 'axios';
+// import '../App.css'
+
+const CharacterWrapper = styled.div`
+    background-color: black;
+    /* display: flex;
+    justify-content: center;
+    flex-direction: column; */
+    text-align: center;
+`;
+
+const CharacterName = styled.h2`
+    color: Yellow;
+`;
+
+const CharacterBio = styled.h4`
+    color: Yellow;
+`;
+
+// function getHomeworld(props) {
+//     axios.get(`${props.homeworld}`)
+//     .then(res => {
+//         props.homeworld = res.data.name
+//         console.log(props.homeworld)
+//     })
+//     .catch(err => {
+//         console.log(err)})
+// }
+
+// function getStarships(props) {
+//     let characterStarships = props.starships
+//     for(let i = 0; i < characterStarships.length; i++) {
+//     // props.starships.forEach(element => {
+//         axios.get(props.starships[i])
+//         .then(res => {
+//             if(i === 0) {
+//                 props.starships = `${res.data.name}`
+//             } else {
+//                 props.starships = props.starships + `, ${res.data.name}`
+//             console.log(props.starships)
+//             }
+//         })
+//         .catch(err => {
+//             console.log(err)})
+//     }
+
+// }
+
+
+function Character(props) {
+    const { character} = props
+
+    // getHomeworld(character)
+    // getStarships(character)
+
+
+    return (
+        <CharacterWrapper key={`${character.name}-wrapper`} id={`${character.name}-wrapper`} className = 'character-wrapper'>
+
+            {console.log(character)}
+
+            <CharacterName key={`${character.name}-name`} id={`${character.name}-name`} className = 'character-name'>{character.name}</CharacterName>
+
+            <CharacterBio key={`${character.name}-bio`} id={`${character.name}-bio`} className = 'character-bio'>
+
+                <p>Films: {character.films}</p>
+
+                <p>Homeworld: {character.homeworld}</p>
+
+                <p>Birth Year: {character.birth_year}</p>
+
+                <p>Starships: {character.starships}</p>
+
+            </CharacterBio>
+
+        </CharacterWrapper>
+    )
+}
+
+export default Character
